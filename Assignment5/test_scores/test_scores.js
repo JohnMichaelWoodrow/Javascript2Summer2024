f31d299678229bd58ece09515022d5bf27022d1c
John-Michael Woodrow <john-michael.woodrow19@ed.cna.nl.ca>
2024-08-08 18:19:42 -0230

    ass5

--- Assignment5/test_scores/test_scores.js
+++ Assignment5/test_scores/test_scores.js
@@ -2,40 +2,36 @@
 
 $(document).ready( () => {
 
-    const scores = [];
-
     $("#add_score").click( () => {
         
         const score = parseFloat($("#score").val());
-                
+
         if (isNaN(score) || score < 0 || score > 100) {
-            $("#add_score").next().text("Score must be from 0 to 100."); 
+            $("#add_score").next().text("Score must be between 0 and 100."); 
         }
         else {
             $("#add_score").next().text("");  
+
             // add score to scores array 
-            scores.push(score);
+            testScores.add(score);
 
-            // display all scores
-            $("#all").text(scores.join(", "));
+            // display all 
+            $("#all").text(testScores.toString());
 
-            // display average score
-            const total = scores.reduce( (tot, val) => tot + val, 0 );
-            const avg = total/scores.length;
-            $("#avg").text(avg.toFixed(2));
+            // display avg
+            $("#avg").text(testScores.avg.toFixed(1));
 
-            // display last 3 scores
-            const len = scores.length;
-            const copy = (len <= 3) ? scores.slice() : scores.slice(len - 3, len); // copy last three
-            copy.reverse();
-            $("#last").text(copy.join(", "));
+            // display last 3
+            $("#last").text(testScores.lastThree);
         }
-        
-        // get text box ready for next entry
+
         $("#score").val("");
         $("#score").focus(); 
-    });
 
-    // set focus on initial load
+        for (const score of testScores) {
+            console.log(score);
+        }
+    });
+    
     $("#score").focus();
 });
